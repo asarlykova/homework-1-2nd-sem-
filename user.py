bed9b4c56334697b2330fca423695ef49f1b0e6d
asarlykova <150665283+asarlykova@users.noreply.github.com>
2024-02-01 22:30:50 +0600

    Add files via upload

---
+++ user.py
@@ -0,0 +1,6 @@
+user={}
+user["What is user's name?"]=input()
+user["What is user's age?"]=input()
+user["What is user's country of birth?"]=input()
+user["What is user known for?"]=input()
+print(user)
